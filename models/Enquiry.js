834cea2b284a0e9c9a8125a17f4ff4f00f2a8f64
Louis <louisreid@gmail.com>
2016-10-26 15:48:20 +0100

    Initial Commmit - working form

---
+++ models/Enquiry.js
@@ -0,0 +1,23 @@
+var keystone = require('keystone'),
+	Types = keystone.Field.Types;
+
+/**
+ * Enquiry Model
+ * =============
+ */
+
+var Enquiry = new keystone.List('Enquiry', {
+	nocreate: true,
+	noedit: true
+});
+
+Enquiry.add({
+	name: { type: Types.Name, required: true },
+	position: { type: String, required: true },
+	email: { type: Types.Email, required: true },
+	createdAt: { type: Date, default: Date.now }
+});
+
+Enquiry.defaultSort = '-createdAt';
+Enquiry.defaultColumns = 'name, email, enquiryType, createdAt';
+Enquiry.register();
